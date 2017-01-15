b0c9265371a819915fd9ef3b70feff5201e73a84
Paroksh <paroksh@Parokshs-MacBook-Pro.local>
2017-01-15 17:32:28 +0530

    minor ui edits

--- public/javascripts/mainpage.js
+++ public/javascripts/mainpage.js
@@ -7,7 +7,7 @@ var MessageBox = React.createClass({
 		time = this.props.messageDetails.time;
 		username = this.props.messageDetails.username;
 		return(
-			<div className="well well-sm center-block " style={{"border-radius" : "10px", "background-color" : "lightgoldenrodyellow"}}>
+			<div className="well well-sm center-block " style={{"border-radius" : "10px", "background-color" : "#fcf8e3"}}>
 				<h5 className="text-center">{message}</h5><small> - {username} ( {time} )</small>
 			</div>
 		)
@@ -151,30 +151,33 @@ var Page = React.createClass({
 
 		return(
 			<div >
-				<div className="navbar navbar-default bg-primary navbar-fixed-top">
+				<div className="navbar navbar-fixed-top">
 					<ul className="nav nav-pills nav-justified bg-info">
 						<li role="presentation" className="col-sm-1" onClick={this.showWishes}><a className="text-success" href="#wishes">Wishes</a></li>
 						<li role="presentation" className="col-sm-1" onClick={this.showTeamStatus}><a className="text-success" href="#teamStatus">Team Status</a></li>
 						<li role="presentation" className="col-sm-1" data-toggle="modal" data-target="#messageModal"><a className="text-success" href="#writeWishes">Write your wishes</a></li>
 					</ul>
-					
-				</div>
-				<div className="well">
-				<p>Two of our teams, <a className="text-info" href="https://trailwalker.oxfamindia.org/TEAM2509">Mission I M Possible </a>
-				( Paroksh, Jainam, Apoorv, Shashank ) and <a className="text-info" href="https://trailwalker.oxfamindia.org/TEAM2450">Game of trails </a> 
-				( Neha, Astha, Shipra, Ankita ) are participating in oxfam. Wish them luck :)</p>
-				<p>You can browse through some of our video/content: 
-				<a className="text-info" href="https://www.youtube.com/watch?v=IQh9vdL_tDo&t=28s">Preparation Video</a>,  
-				<a className="text-info" href="https://medium.com/@parokshsaxena/the-reason-behind-our-mission-100-km-oxfam-walk-26d3df622969#.nf9d7794o">Our motivation</a>				
-				</p>
-				</div>
-				<div className="bg-warning">
-					{rows}
 				</div>
 
-				<div id="messageModal" className="modal fade" role="dialog">
-				 <CaptureMessageBox />
-				</div>	
+				<div className="well" style={{"padding-top":"60px"}}>
+					<b>
+						<p>Two of our teams, <a className="text-info" href="https://trailwalker.oxfamindia.org/TEAM2509">Mission I M Possible </a>
+						( Paroksh, Jainam, Apoorv, Shashank ) and <a className="text-info" href="https://trailwalker.oxfamindia.org/TEAM2450">Game of Trails </a> 
+						( Neha, Astha, Shipra, Ankita ) are participating in Oxfam 2017, 19 Jan. Wish them luck :)</p>
+						<p>You can also browse through some of our video/content : <a className="text-info" href="https://www.youtube.com/watch?v=IQh9vdL_tDo&t=28s">Preparation Video</a>, <a className="text-info" href="https://medium.com/@parokshsaxena/the-reason-behind-our-mission-100-km-oxfam-walk-26d3df622969#.nf9d7794o">Our motivation</a>				
+						</p>
+					</b>
+				</div>
+
+				<div style={{"padding-left":"40px","padding-right":"40px"}}>
+					<div>
+						{rows}
+					</div>
+
+					<div id="messageModal" className="modal fade" role="dialog">
+					 <CaptureMessageBox />
+					</div>	
+				</div>
 			</div>
 
 		)
